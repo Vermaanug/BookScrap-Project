361900abc9b2f11631f8dd4f8060f9422983c9c9
Vermaanug <Vermaanug1974@gmail.com>
2023-09-20 23:44:01 +0530

    Add Book Schema for Storing Book information

---
+++ src/models/Book.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const bookSchema = mongoose.Schema({
+    bookname: { 
+        type: String, 
+        required: true 
+    },
+    standard: { 
+        type: String, 
+        required: true 
+    },
+    number: { 
+        type: String, 
+        required: true 
+    },
+    location: { 
+        type: String, 
+        required: true 
+    },
+    pincode: { 
+        type: String, 
+        required: true 
+    },
+    imagePath: { 
+        type: String 
+    } 
+});
+
+const Book = new  mongoose.model('Book', bookSchema);
+
+module.exports = Book;
