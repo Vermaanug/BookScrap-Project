d8c38851e93ce30ba15e00ce0d62eded0aeee785
Vermaanug <Vermaanug1974@gmail.com>
2023-10-01 23:40:25 +0530

    Modified User schema to include Firstname and Lastname fields

--- src/models/Signup.js
+++ src/models/Signup.js
@@ -1,6 +1,14 @@
 const mongoose = require("mongoose")
 
 const SignUp = mongoose.Schema({
+    Firstname:{
+        type:String,
+        require:true
+    },
+    Lastname:{
+        type:String,
+        require:true
+    },
     Username:{
         type:String,
         require:true
@@ -12,7 +20,7 @@ const SignUp = mongoose.Schema({
     Password:{
         type:String,
         require:true
-    } 
+    }
 })
 
 const User = new mongoose.model('User', SignUp);
